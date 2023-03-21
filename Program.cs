91c0fa9fc3a8fa5e0f5df91de8602293b790265c
lamlaj <lamlajali@gmail.com>
2023-03-21 17:22:48 +0000

    Initial app commit

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿using System.Drawing;
+using Console = Colorful.Console;
+
+// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Molo, Mhlobo!");
+Console.WriteAscii("Molo, Mhlobo!", Color.Green);
